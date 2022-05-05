05bce828660c89e3499add00c6d6e47b8ffb0178
Pravin <pravinsharma05298@gmail.com>
2022-05-06 03:58:17 +0530

    displaying data and ui

--- js/apicall.js
+++ js/apicall.js
@@ -6,10 +6,23 @@ $(document).ready(function(){
         const title = $(".my-title").val()
         if (title !== '') {
             console.log(title);
+            content = tinymce.get('text').getContent({format: 'text'});
+            // console.log(tinymce.activeEditor.getContent())
+            console.log('This is content', content)
             callAPI(title);
+            $('#text').val()
         } else {
             console.log('title is empty');
         }
+    });
+    $(document).ajaxStart(function () {
+        $("#spinner").show();
+        $("#spinner-text").hide();
+    });
+  
+    $(document).ajaxStop(function () {
+        $("#spinner").hide();
+        $("#spinner-text").show();
     });
   });
 
@@ -18,13 +31,18 @@ $(document).ready(function(){
 
 function callAPI(title) {
     $.ajax({
-        url: 'https://l3y9dqs7gc.execute-api.us-west-2.amazonaws.com/dev',
+        url: 'https://7j1jbr1066.execute-api.us-west-2.amazonaws.com/dev',
         type: 'POST',
         dataType: 'json',
-        data: {'prompt': title},
+        data: JSON.stringify({"prompt": title}),
+        headers : {"Content-Type": "application/json"},
         success: (result, status, xhr)=> {
-            console.log(result);
-            console.log(typeof(result));
+            console.log('This is result', result);
+            // console.log(JSON.parse(result))
+            // console.log(JSON.parse(result))
+            tinymce.get('text').setContent(result['text'])
+            // console.log($('#text').val())
+
         },
         error: (xhr, status, error)=> {
             alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText);
