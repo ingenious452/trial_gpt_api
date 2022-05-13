0cf5b30284e5a767b5a536d8df9fa568ff286e0d
Pravin <pravinsharma05298@gmail.com>
2022-05-13 15:27:30 +0530

    Change the model being used to text-davinci-001

--- js/apicall.js
+++ js/apicall.js
@@ -4,12 +4,14 @@
 $(document).ready(function(){
     $(".btn").click(function(){
         const title = $(".my-title").val()
+        let previousTitle = ''
         if (title !== '') {
             // console.log(title);
             const content = tinymce.get('text').getContent({format: 'text'});
+            console.log(content)
             // console.log(tinymce.activeEditor.getContent())
             // console.log('This is content', content)
-            callAPI(title, content);
+            callAPI(previousTitle, title, content);
 
         } else {
             console.log('title is empty');
@@ -29,21 +31,34 @@ $(document).ready(function(){
 
 
 
-function callAPI(title, content) {
+function callAPI(previousTitle, title, content) {
     $.ajax({
         url: 'https://46rc60js1j.execute-api.us-west-2.amazonaws.com/prod',
         type: 'POST',
         dataType: 'json',
-        data: JSON.stringify({"body": {"model_name": "davinci","title": title, "content": content}}),
+        data: JSON.stringify({"body": {"model_name": "text-davinci-001","title": title, "content": content}}),
         headers : {"Content-Type": "application/json"},
         success: (result, status, xhr)=> {
+   
             console.log('This is result', result);
             output = JSON.parse(result['body'])
 
             if (result['statusCode'] != 200) {
                 alert(output['error'])
+            } else {
+                
+                output['content'] = output['content'].replace(/\n\n/g, '<br><br>')
+                if (content == "") {
+                    console.log('Only Title was provided')
+                    console.log(output['content'])
+                    tinymce.get('text').setContent(output['content'])
+                } else {
+                    console.log('Title and content was provide')
+                    console.log(output['content'])
+                    tinymce.get('text').setContent(content + '<br><br>' + output['content'])
+
+                }
             }
-            tinymce.get('text').setContent(output['output'])
         },
         error: (xhr, status, error)=> {
             alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText);
